e06edc580e45aa1cbed11c796147a1fbe7cbdb37
ScottBot10 <46166818+ScottBot10@users.noreply.github.com>
2023-11-10 17:08:08 +0200

    Updated zip_release.sh to the use constants.py instead of search_options.py

--- zip_release.sh
+++ zip_release.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 version=$(grep 'version' __init__.py | sed -r "s/^.*version\s*= \(([0-9]+), ([0-9]+), ([0-9]+)\).*/\1.\2.\3/")
-zip "calibre_annas_archive-v${version}.zip" README.md plugin-import-name-store_annas_archive.txt __init__.py annas_archive.py config.py search_options.py
+zip "calibre_annas_archive-v${version}.zip" README.md plugin-import-name-store_annas_archive.txt __init__.py annas_archive.py config.py constants.py
